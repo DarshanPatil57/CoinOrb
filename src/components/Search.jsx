7b6a8c81d35052afcdd7840507ac6beefe74fb23
Darshan Patil <5303darshan@gmail.com>
2024-07-22 15:27:57 +0530

    feat:implemented search feature

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,32 +1,80 @@
-import React, { useState } from 'react'
-import searchIcon from "../assets/search-icon.svg"
+import React, { useContext, useState } from "react";
+import searchIcon from "../assets/search-icon.svg";
+import { CryptoContext } from "../context/CryptoContext";
+import debounce from "debounce";
+
+const SearchInput = ({ handleSearch }) => {
+  const [searchText, setSearchText] = useState("");
+
+  let { searchData,setCoinSearchData ,setSearchData} = useContext(CryptoContext);
+
+  const handleInput = (e) => {
+    let query = e.target.value;
+    setSearchText(query);
+    handleSearch(query);
+    // console.log(query);
+  };
+
+  const selectCoin = (coins) => {
+    setCoinSearchData(coins)
+    setSearchText(" ")
+    setSearchData([])
+  }
+  return (
+    <>
+      <form className="w-96 relative flex items-center ml-7 font-nunito" onSubmit={(e) => e.preventDefault()}>
+        <input
+          onChange={handleInput}
+          value={searchText}
+          type="text"
+          name="search"
+          placeholder="Search Here ..."
+          className="w-full rounded bg-gray-200 placeholder:text-gray-100 pl-2  required outline-0 border border-transparent focus:border-cyan"
+        />
+        <button type="submit" className="absolute right-1 cursor-pointer">
+          <img src={searchIcon} className="w-full h-auto" alt="search" />
+        </button>
+      </form>
+
+      {searchText.length > 0 ? (
+        <ul className="absolute top-11 right-0 w-96 h-96 rounded overflow-x-hidden py-2 bg-gray-200 bg-opacity-60 backdrop-blur-md">
+          {searchData ? (
+            searchData.map(coins => { 
+              return <li className="flex items-center ml-4 my-2 cursor-pointer"
+               key={coins.id}
+               onClick={()=>selectCoin(coins.id)}
+               >
+                 <img 
+                      className="w-[1rem] h-[1rem] mx-1.5"
+                      src={coins.thumb}
+                      alt={coins.name}
+                    />
+                    <span>{coins.name}</span>
+              </li>;
+            })
+          ) : (
+            <h2>Please wait ...</h2>
+          )}
+        </ul>
+      ) : null}
+    </>
+  );
+};
 
 const Search = () => {
-    const [searchText,setSearchText] = useState()
+  let { getSearchResult } = useContext(CryptoContext);
 
-    const handleInput = (e)=>{
-        e.preventDefault()
-        let query = e.target.value
-        setSearchText(query)
-        // console.log(query);
+  //   debounce function for search
 
-    }
+  const debounceFunc = debounce(function (val) {
+    getSearchResult(val);
+  }, 500);
+
   return (
-  <>
-    <form className='w-96 relative flex items-center ml-7 font-nunito'>
-        <input onChange={handleInput} value={searchText} type="text" name='search' placeholder='Search Here ...'   className='w-full rounded bg-gray-200 placeholder:text-gray-100 pl-2 required outline-0 border border-transparent focus:border-cyan' />
-        <button type='submit' className='absolute right-1 cursor-pointer'>
-            <img src={searchIcon} className='w-full h-auto' alt="search" />
-        </button>
-    </form>
-    {/* {
-        searchText.length > 0 ? 
-        <ul>
-            <li>Bitcon</li>
-        </ul> : null
-    } */}
-  </>
-  )
-}
+    <div className="relative">
+      <SearchInput handleSearch={debounceFunc} />
+    </div>
+  );
+};
 
-export default Search
+export default Search;
--- src/context/CryptoContext.jsx
+++ src/context/CryptoContext.jsx
@@ -10,15 +10,30 @@ export const CryptoContext = createContext({
 // create context provider 
 
 export const CryptoProvider = ({children}) =>{
-    const [cryptoData,setCryptoData] = useState()
+    const [cryptoData,setCryptoData] = useState([])
+    const [searchData,setSearchData] = useState([])
+    const [coinsearchData,setCoinSearchData] = useState([])
 
     const getCryptoData = async () => {
         try {
             const data = await fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d`)
             .then(res => res.json())
             .then(json => json)
-            console.log(data);
+            // console.log(data);
             setCryptoData(data)
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    const getSearchResult = async (query) => {
+        try {
+            const data = await fetch(`https://api.coingecko.com/api/v3/search?query=${query}`)
+            .then(res => res.json())
+            .then(json => json)
+            // console.log(data);
+            setSearchData(data.coins)
+
         } catch (error) {
             console.log(error);
         }
@@ -26,10 +41,10 @@ export const CryptoProvider = ({children}) =>{
 
     useLayoutEffect(() => {
         getCryptoData()
-    }, [])
+    }, [coinsearchData])
     
     return (
-        <CryptoContext.Provider value={{cryptoData}}>
+        <CryptoContext.Provider value={{cryptoData,searchData,getSearchResult,coinsearchData,setCoinSearchData,setSearchData}}>
             {children}
         </CryptoContext.Provider>
     )

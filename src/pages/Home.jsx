62e580ac728f39090890b1147cc5d0f1ac6fa856
Darshan Patil <5303darshan@gmail.com>
2024-07-28 20:07:53 +0530

    feat:added trending component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,16 +3,19 @@ import { Outlet } from "react-router-dom";
 import Logo from "../components/Logo";
 import Navigation from "../components/Navigation";
 import { CryptoProvider } from "../context/CryptoContext";
+import { TrendingProvider } from "../context/TrendingContex";
 
 const Home = () => {
   return (
     <CryptoProvider>
+      <TrendingProvider>
       <main className=" w-full h-full flex flex-col content-center items-center relative text-white font-nunito">
         <div className="w-screen h-screen bg-gray-300 fixed -z-10"></div>
         <Logo />
         <Navigation />
         <Outlet />
       </main>
+      </TrendingProvider>
     </CryptoProvider>
   );
 };
